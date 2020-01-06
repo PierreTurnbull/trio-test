e7a38c30153d91ab380237300c07ee8401a29e47
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-01-06 22:31:49 +0100

    Fixed .travis.yml.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-echo ok
+yes yes | openssl aes-256-cbc -K $encrypted_34a133650a4c_key -iv $encrypted_34a133650a4c_iv -in private_key.enc -out ./private_key -d
+yes yes | scp -i ./private_key ./docker-compose.yml ubuntu@35.180.25.176:/home/ubuntu/docker-compose.yml
+yes yes | scp -i ./private_key ./api/dump.sql ubuntu@35.180.25.176:/home/ubuntu/dump.sql
