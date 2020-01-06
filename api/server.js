4762ae9f0de6a1243f5f60dd0e80c9cbfccb3c9c
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-01-06 11:35:46 +0100

    Init.

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mysql = require('mysql');
+const app = express()
+
+const connection = mysql.createConnection({
+  host: 'trio-test-api-bdd',
+  port: 3306,
+  user: 'trio',
+  password: 'trio',
+  database: 'trio'
+});
+connection.connect();
+
+app.get('/', function (req, res) {
+  res.send('Ceci est mon API !!!!!!!!!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
