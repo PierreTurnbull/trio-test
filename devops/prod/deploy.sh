62c03cb6067d44fae71095d198fb907bc0653001
Pierre Turnbull <33256529+PierreTurnbull@users.noreply.github.com>
2020-01-28 11:25:21 +0100

    Added server install

--- devops/prod/deploy.sh
+++ devops/prod/deploy.sh
@@ -1,3 +1,17 @@
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 sudo docker-compose down --rmi all
 sudo docker system prune -f -a
 sudo docker-compose up -d
